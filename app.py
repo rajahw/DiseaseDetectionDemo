89dbff299417b3c006147c3e0785140f5bfa67e3
rajahw <rajahwilliams77@gmail.com>
2025-11-15 17:08:05 -0600

    Additions

--- app.py
+++ app.py
@@ -13,6 +13,23 @@ def analyze(uploaded_image, disease):
 st.set_page_config(layout="wide", page_title="Disease Detection")
 st.title("Disease Detection")
 
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-5.0-mini"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
 imageUploaded = False
 disease = 0
 
